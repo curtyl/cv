35dc818b71359329e19e0d107afdce27d73812aa
Louis Curty <Louis.Curty@etu.univ-grenoble-alpes.fr>
2016-11-28 19:09:00 +0100

    navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,26 @@ $(document).ready(function() {
   });
 
   $(window).bind('scroll', function() {
-    if($(window).scrollTop() > 69.75){
+    if($(window).scrollTop() > 69){
       $('.Dropdown').addClass('fixed');
     }else{
       $('.Dropdown').removeClass('fixed');
     }
   })
+
+  $(".formations").on("click",function(){
+    $('html, body').animate({scrollTop:$('#Formations').position().top}, 'slow');
+  });
+
+  $(".ep").on("click",function(){
+    $('html, body').animate({scrollTop:$('#ep').position().top}, 'slow');
+  });
+
+  $(".profil").on("click",function(){
+    $('html, body').animate({scrollTop:$('#profil').position().top}, 'slow');
+  });
+
+  $(".contact").on("click",function(){
+    $('html, body').animate({scrollTop:$('#contact').position().top}, 'slow');
+  });
 });
