9991f9fef377eabf14d567ead5bf2fe40cad5610
Louis Curty <Louis.Curty@etu.univ-grenoble-alpes.fr>
2016-11-28 16:37:34 +0100

    fonction pour fixe menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-$(".Dropdown").on("click",function(){
-  $(this).toggleClass('is-expanded');
+$(document).ready(function() {
+  $(".Dropdown").on("click",function(){
+    $(this).toggleClass('is-expanded');
+  });
+
+  $(window).bind('scroll', function() {
+    if($(window).scrollTop() > 69.75){
+      $('.Dropdown').addClass('fixed');
+    }else{
+      $('.Dropdown').removeClass('fixed');
+    }
+  })
 });
